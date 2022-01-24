9951bdc5cfc6423c6e7fb1333f834978a5988526
NnickozZ <98185606+NnickozZ@users.noreply.github.com>
2022-01-24 21:20:38 +0100

    Changed name of spike script
    [*] Changed name because I will use this for more then just spikes

--- ProjectNinobiGGJ2022/Assets/Scripts/Obstacles/Killbox.cs
+++ ProjectNinobiGGJ2022/Assets/Scripts/Obstacles/Killbox.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Spike : MonoBehaviour
+public class Killbox : MonoBehaviour
 {
 
     private GameObject[] players;
